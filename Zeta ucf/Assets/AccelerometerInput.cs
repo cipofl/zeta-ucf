94fb19d80ca968d179e2d1e50818300e5fe75ef7
DESKTOP-RCS8KTK\cflorea <Ciprian.Florea@oulu.fi>
2018-10-22 16:19:09 +0300

    Acceleration

--- Zeta ucf/Assets/AccelerometerInput.cs
+++ Zeta ucf/Assets/AccelerometerInput.cs
@@ -1,19 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class AccelerometerInput : MonoBehaviour
 {
 
+    public GameObject textMesh;
+    public GameObject toggle;
+
     // Use this for initialization
     void Start()
     {
-
+        textMesh = GameObject.Find("TextMeshPro Text");
+        toggle = GameObject.Find("Toggle");
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(Input.acceleration);
+        if (toggle.GetComponent<Toggle>().isOn)
+        {
+            textMesh.GetComponent<TextMeshProUGUI>().text = "Acceleration is\n" + Input.acceleration.ToString();
+        }
     }
 }
