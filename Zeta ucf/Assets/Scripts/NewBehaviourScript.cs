54a052d22c8ccd672c8d91e7f284c6e89cc45634
Ciprian <>
2019-04-24 10:58:06 +0300

    new scene with realod and quit buttons

---
+++ Zeta ucf/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Reload
+    public void Reload()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Quit
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
