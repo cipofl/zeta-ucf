973c6a22aa71ebb3d552599ad890c5895a7f1001
Ciprian <>
2019-04-24 11:21:51 +0300

    a

--- Zeta ucf/Assets/Scripts/NewBehaviourScript.cs
+++ Zeta ucf/Assets/Scripts/NewBehaviourScript.cs
@@ -29,4 +29,9 @@ public class NewBehaviourScript : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void LoadTakeTestScene()
+    {
+        SceneManager.LoadScene("Take Test Scene");
+    }
 }
