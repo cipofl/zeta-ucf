c647b977dd77b3e6e89528afd9fccbd4ef2d6bae
Ciprian <>
2019-04-25 16:38:52 +0300

    pwp api

--- Zeta ucf/Assets/Scripts/NewBehaviourScript.cs
+++ Zeta ucf/Assets/Scripts/NewBehaviourScript.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
 
+    public GameObject panel, panel2;
+
+    private string getForUserEndPoint = "http://localhost:5000/api/LimpingTests/GetForUser/";
+    private string getByIdEndPoint = "http://localhost:5000/api/LimpingTests/GetById/";
+    private string createEndPoint = "http://localhost:5000/api/LimpingTests/Create";
+    private string editEndPoint = "http://localhost:5000/api/LimpingTests/Edit/";
+    private string deleteEndPoint = "http://localhost:5000/api/LimpingTests/Delete/";
+
     // Use this for initialization
     void Start()
     {
-
+        panel.SetActive(true);
+        panel2.SetActive(false);
     }
 
     // Update is called once per frame
@@ -33,5 +44,59 @@ public class NewBehaviourScript : MonoBehaviour
     public void LoadTakeTestScene()
     {
         SceneManager.LoadScene("Take Test Scene");
+    }
+
+
+    // API Methods
+    public void GetForUser()
+    {
+        GameObject inputField = GameObject.Find("InputField userId");
+        string userId = inputField.GetComponent<InputField>().text;
+        print("GetForUser()" + " " + userId);
+
+        StartCoroutine(GetText(getForUserEndPoint + userId));
+    }
+
+    public void GetById()
+    {
+        GameObject inputField = GameObject.Find("InputField limpingTestId");
+        string limpingTestId = inputField.GetComponent<InputField>().text;
+        print("GetById()" + " " + limpingTestId);
+
+        StartCoroutine(GetText(getByIdEndPoint + limpingTestId));
+    }
+
+    public void Create()
+    {
+        print("Create()");
+    }
+
+    public void Edit()
+    {
+        print("Edit()");
+    }
+
+    public void Delete()
+    {
+        print("Delete()");
+    }
+
+    IEnumerator GetText(string resource)
+    {
+        UnityWebRequest www = UnityWebRequest.Get(resource);
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
     }
 }
