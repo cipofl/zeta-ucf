eff19761e2665e20a40140ed64fdff052edd2762
flciprian <flciprian262@gmail.com>
2019-05-09 16:53:55 +0300

    asdasd

--- Zeta ucf/Assets/Scripts/NewBehaviourScript1.cs
+++ Zeta ucf/Assets/Scripts/NewBehaviourScript1.cs
@@ -15,6 +15,12 @@ public class NewBehaviourScript1 : MonoBehaviour
     private string limpingTestsSingleHref;
     private string limpingTestsCreate;
     private User user;
+    private MyTestAnalysis testAnalysis;
+    private MyLimpingTest limpingTest;
+    private string appUserid;
+    private string limpingTestsEditHref;
+    private string limpingTestsDeleteHref;
+    private string limpingTestid;
 
     private void Start()
     {
@@ -82,8 +88,10 @@ public class NewBehaviourScript1 : MonoBehaviour
             print(limpingTestsSingleHref);
 
             GameObject.Find("Panel api (1)/id").GetComponent<Text>().text = "Id: " + dynamic["id"].ToString();
+            appUserid = dynamic["id"].ToString();
             GameObject.Find("Panel api (1)/username").GetComponent<Text>().text = "Username: " + dynamic["userName"].ToString();
             GameObject.Find("Panel api (1)/email").GetComponent<Text>().text = "Email: " + dynamic["email"].ToString();
+
         }
     }
 
@@ -112,6 +120,117 @@ public class NewBehaviourScript1 : MonoBehaviour
         limpingTestsCreate = host + dynamic["_links"]["limpingTests-create"]["href"];
         print(limpingTestsCreate);
     }
+
+    //Create a limping test
+    public IEnumerator CreateLimpingTest()
+    {
+        yield return StartCoroutine(GetLimpingTestsForUser());
+
+        string testData = GameObject.Find("testData").GetComponent<InputField>().text;
+        int endValue = int.Parse(GameObject.Find("endValue").GetComponent<InputField>().text);
+        string description = GameObject.Find("description").GetComponent<InputField>().text;
+        int limpingSeverity = int.Parse(GameObject.Find("limpingSeverity").GetComponent<InputField>().text);
+
+        testAnalysis = new MyTestAnalysis
+        {
+            endValue = endValue,
+            description = description,
+            limpingSeverity = limpingSeverity
+        };
+
+        limpingTest = new MyLimpingTest
+        {
+            appUserId = appUserid,
+            testData = testData,
+            testAnalysis = testAnalysis
+        };
+
+        string postData = JsonUtility.ToJson(limpingTest);
+
+        var request = new UnityWebRequest(limpingTestsCreate, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(postData);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.Send();
+
+        Debug.Log("Status Code: " + request.responseCode);
+
+        dynamic dynamic = JsonConvert.DeserializeObject(request.downloadHandler.text);
+        print(dynamic);
+
+        if (request.responseCode == 200)
+        {
+            GameObject.Find("Panel api (2)").SetActive(false);
+            GameObject.Find("Panel").transform.Find("Panel api (3)").gameObject.SetActive(true);
+            limpingTestsEditHref = host + dynamic["_links"]["limpingTests-edit"]["href"];
+            limpingTestsDeleteHref = host + dynamic["_links"]["limpingTests-delete"]["href"];
+            print(limpingTestsEditHref);
+
+            GameObject.Find("Panel api (3)/id").GetComponent<Text>().text = "Id: " + dynamic["id"].ToString();
+            limpingTestid = dynamic["id"].ToString();
+            GameObject.Find("Panel api (3)/date").GetComponent<Text>().text = "Date: " + dynamic["date"].ToString();
+            GameObject.Find("Panel api (3)/testdata").GetComponent<Text>().text = "Test data: " + dynamic["testData"].ToString();
+            GameObject.Find("Panel api (3)/appuserid").GetComponent<Text>().text = "App user id: " + dynamic["appUserId"].ToString();
+        }
+    }
+
+    public IEnumerator DeleteLimpingTest()
+    {
+        UnityWebRequest www = UnityWebRequest.Delete(limpingTestsDeleteHref);
+        yield return www.SendWebRequest();
+
+        Debug.Log("Status Code: " + www.responseCode);
+
+        if (www.responseCode == 200)
+        {
+            GameObject.Find("Panel api (3)").SetActive(false);
+            GameObject.Find("Panel").transform.Find("Panel api (4)").gameObject.SetActive(true);
+        }
+    }
+
+    public IEnumerator EditLimpingTest()
+    {
+        string testData = GameObject.Find("testData").GetComponent<InputField>().text;
+        int endValue = int.Parse(GameObject.Find("endValue").GetComponent<InputField>().text);
+        string description = GameObject.Find("description").GetComponent<InputField>().text;
+        int limpingSeverity = int.Parse(GameObject.Find("limpingSeverity").GetComponent<InputField>().text);
+
+        MyTestAnalysis testAnalysis = new MyTestAnalysis
+        {
+            endValue = endValue,
+            description = description,
+            limpingSeverity = limpingSeverity
+        };
+
+        MyLimpingTest2 limpingTest = new MyLimpingTest2
+        {
+            testData = testData,
+            testAnalysis = testAnalysis
+        };
+
+        string postData = JsonUtility.ToJson(limpingTest);
+
+        var request = new UnityWebRequest(limpingTestsEditHref, "PATCH");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(postData);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.Send();
+
+        Debug.Log("Status Code: " + request.responseCode);
+
+        dynamic dynamic = JsonConvert.DeserializeObject(request.downloadHandler.text);
+        print(dynamic);
+
+        if (request.responseCode == 200)
+        {
+            GameObject.Find("Panel api (5)").SetActive(false);
+            GameObject.Find("Panel").transform.Find("Panel api (6)").gameObject.SetActive(true);
+        }
+    }
 }
 
 [System.Serializable]
@@ -121,4 +240,35 @@ public class User
     public string userName;
     [SerializeField]
     public string email;
+}
+
+[System.Serializable]
+public class MyLimpingTest
+{
+    [SerializeField]
+    public string appUserId;
+    [SerializeField]
+    public string testData;
+    [SerializeField]
+    public MyTestAnalysis testAnalysis;
+}
+
+[System.Serializable]
+public class MyTestAnalysis
+{
+    [SerializeField]
+    public int endValue;
+    [SerializeField]
+    public string description;
+    [SerializeField]
+    public int limpingSeverity;
+}
+
+[System.Serializable]
+public class MyLimpingTest2
+{
+    [SerializeField]
+    public string testData;
+    [SerializeField]
+    public MyTestAnalysis testAnalysis;
 }
