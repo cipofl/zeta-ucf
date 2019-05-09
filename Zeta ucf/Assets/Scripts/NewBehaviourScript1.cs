3d16a0c1eb898eb7ea44c49dfd4da18ec760e32a
flciprian <flciprian262@gmail.com>
2019-05-09 14:33:19 +0300

    Create user

--- Zeta ucf/Assets/Scripts/NewBehaviourScript1.cs
+++ Zeta ucf/Assets/Scripts/NewBehaviourScript1.cs
@@ -6,6 +6,7 @@ using UnityEngine.Networking;
 using UnityEngine.UI;
 using Limping.Api.Models;
 using Newtonsoft.Json;
+using System.Text;
 
 public class NewBehaviourScript1 : MonoBehaviour
 {
@@ -17,6 +18,7 @@ public class NewBehaviourScript1 : MonoBehaviour
         StartCoroutine(GetRoot());
     }
 
+    //Gets all the links you can navigate to from the root
     public IEnumerator GetRoot()
     {
         UnityWebRequest www = UnityWebRequest.Get(host + "/api/root");
@@ -41,4 +43,44 @@ public class NewBehaviourScript1 : MonoBehaviour
         usersCreateHref = host + dynamic["_links"]["users-create"]["href"];
         print(usersCreateHref);
     }
+
+    //Create a user and then return the dto
+    public IEnumerator CreateUser()
+    {
+        string userName = GameObject.Find("user name input field").GetComponent<InputField>().text;
+        string email = GameObject.Find("email input field").GetComponent<InputField>().text;
+
+        User user = new User
+        {
+            userName = userName,
+            email = email
+        };
+
+        string postData = JsonUtility.ToJson(user);
+
+        var request = new UnityWebRequest(usersCreateHref, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(postData);
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+
+        yield return request.Send();
+
+        Debug.Log("Status Code: " + request.responseCode);
+        print(JsonConvert.DeserializeObject(request.downloadHandler.text));
+
+        if (request.responseCode == 200)
+        {
+            GameObject.Find("Panel api").SetActive(false);
+        }
+    }
+}
+
+[System.Serializable]
+public class User
+{
+    [SerializeField]
+    public string userName;
+    [SerializeField]
+    public string email;
 }
