acd837aae92b77f3a4635fcbfc1266342abf3b68
flciprian <flciprian262@gmail.com>
2019-05-09 13:54:04 +0300

    get root

--- Zeta ucf/Assets/Scripts/NewBehaviourScript1.cs
+++ Zeta ucf/Assets/Scripts/NewBehaviourScript1.cs
@@ -1,16 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using Limping.Api.Models;
+using Newtonsoft.Json;
 
-public class NewBehaviourScript1 : MonoBehaviour {
+public class NewBehaviourScript1 : MonoBehaviour
+{
+    private readonly string host = "http://192.168.99.100:8090";
+    private string usersCreateHref;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Start()
+    {
+        StartCoroutine(GetRoot());
+    }
+
+    public IEnumerator GetRoot()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(host + "/api/root");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+            yield break;
+        }
+        else
+        {
+            // Show results as text
+            //Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+        }
+
+        dynamic dynamic = JsonConvert.DeserializeObject(www.downloadHandler.text);
+        print(dynamic);
+        usersCreateHref = host + dynamic["_links"]["users-create"]["href"];
+        print(usersCreateHref);
+    }
 }
